ade2f05ef930f3e479b0a118511347bf09b6cd28
how101081 <hlpss1010@gmail.com>
2025-12-30 12:11:36 +0800

    add _co

--- homework/4/mult/Mult.asm
+++ homework/4/mult/Mult.asm
@@ -8,3 +8,26 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+@R2
+M=0
+(LOOP)
+  @R1
+  D=M
+  @END
+  D;JEQ   // if(R1==0) goto END
+  
+  @R0
+  D=M       // D = R0
+  @R2
+  M=M+D     // R2 = R2+ R0
+  
+  @R1
+  M=M-1     // R1--
+  
+  @LOOP
+  0;JMP     // goto LOOP
+
+(END)
+
+  @END
+  0;JMP // infinite loop
